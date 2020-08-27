6dc3cfde147ea05215f69dccbbaf222dcc347660
HeeWoong97 <ungung97@naver.com>
2020-08-27 22:26:05 +0900

    2020.08.27 22:26

--- codes/module/readtest.py
+++
@@ -1,9 +0,0 @@
-f = open("detect.txt", "rw")
-
-for i in range(2):
-    if f.readline() == "detect!\n":
-        print("read \"detect!\"")
-    else:
-        print("something not \"detect!\"")
-
-f.close()
--- codes/module/test/send_test.py
+++ codes/module/test/send_test.py
@@ -3,7 +3,7 @@ FIFO_FILENAME = './fifo.txt'
 fp_fifo = open(FIFO_FILENAME, 'w')
 
 for i in range(128):
-    fp_fifo.write("Hello,MakeCode\n")
+    fp_fifo.write("Send Test {}\n".format(i))
     fp_fifo.write("")
 
 fp_fifo.close()
